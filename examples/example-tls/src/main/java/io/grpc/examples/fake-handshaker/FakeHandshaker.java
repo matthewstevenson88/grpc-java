c8c2a4ceb353d1e617ac7b8a9bf241d3488535cd
Davis Gu <gud@google.com>
2020-08-17 21:13:04 +0000

    cleaned up code

--- examples/example-tls/src/main/java/io/grpc/examples/fake-handshaker/FakeHandshaker.java
+++ examples/example-tls/src/main/java/io/grpc/examples/fake-handshaker/FakeHandshaker.java
@@ -98,13 +98,12 @@ public class FakeHandshaker {
           args = defaultArgs;
         }
 
-        FakeHandshaker server =  new FakeHandshaker(Integer.parseInt(args[0]));
+        FakeHandshaker server = new FakeHandshaker(Integer.parseInt(args[0]));
         server.start();
         server.blockUntilShutdown();
     }
 
     private static class FakeS2AImpl extends S2AServiceGrpc.S2AServiceImplBase {
-
       private int state;
       private Identity peerIdentity;
       private Identity localIdentity;
@@ -112,172 +111,169 @@ public class FakeHandshaker {
 
       private final String grpcAppProtocol = "grpc";
       private final String clientHelloFrame = "ClientHello";
-     private final String clientFinishedFrame = "ClientFinished";
+      private final String clientFinishedFrame = "ClientFinished";
       private final String serverFrame = "ServerHelloAndFinished";
 
-     private final String inKey  = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk";
-     private final String outKey = "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj";
+      private final String inKey  = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk";
+      private final String outKey = "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj";
 
-        @Override
-        public StreamObserver<SessionReq> setUpSession(final StreamObserver<SessionResp> stream) {
-            return new StreamObserver<SessionReq>(){
-              @Override
-              public void onNext(SessionReq req){
-                SessionResp resp = null;
-                if (req.getClientStart()!=null){
-                    resp = processClientStart(req);
-                  }
-                  else if (req.getServerStart()!=null){
-                    resp = processServerStart(req);
-                  } else if (req.getNext()!=null){ 
-                    resp = processNext(req);
-                  }
-                  else{
-                    logger.log(Level.WARNING,"Session request has unexpected type: "+req.getClass());
-                  }
+      @Override
+      public StreamObserver<SessionReq> setUpSession(final StreamObserver<SessionResp> stream) {
+          return new StreamObserver<SessionReq>(){
+            @Override
+            public void onNext(SessionReq req){
+              SessionResp resp = null;
+              if (req.getClientStart() != null){
+                resp = processClientStart(req);
+              } else if (req.getServerStart() != null){
+                resp = processServerStart(req);
+              } else if (req.getNext() != null){ 
+                resp = processNext(req);
+              } else {
+                logger.log(Level.WARNING,"Session request has unexpected type: "+req.getClass());
+              }
                 
-                stream.onNext(resp);
-              }
+              stream.onNext(resp);
+            }
 
-              @Override
-              public void onError(Throwable t) {
-                logger.log(Level.WARNING, "Encountered error in routeChat", t);
-              }
+            @Override
+            public void onError(Throwable t) {
+              logger.log(Level.WARNING, "Encountered error in routeChat", t);
+            }
 
-              @Override
-              public void onCompleted() {
-                stream.onCompleted();
-              }
+            @Override
+            public void onCompleted() {
+              stream.onCompleted();
+            }
 
-              private SessionResp processClientStart(SessionReq req){
-                SessionResp.Builder resp = SessionResp.newBuilder();
-                if (state!=0){
-                  resp.setStatus(SessionStatus.newBuilder().setCode(Code.FAILED_PRECONDITION.value())
-                    .setDetails("client start handshaker not in initial state").build());
+            private SessionResp processClientStart(SessionReq req){
+              SessionResp.Builder resp = SessionResp.newBuilder();
+              if (state != 0){
+                resp.setStatus(SessionStatus.newBuilder().setCode(Code.FAILED_PRECONDITION.value())
+                  .setDetails("client start handshaker not in initial state").build());
+                return resp.build();
+              }
+              if (req.getClientStart().getApplicationProtocolsList().size() != 1 ||
+                req.getClientStart().getApplicationProtocols(0) != grpcAppProtocol){
+                  resp.setStatus(SessionStatus.newBuilder().setCode(Code.INVALID_ARGUMENT.value())
+                    .setDetails("application protocol was not grpc").build());
                   return resp.build();
                 }
-                if (req.getClientStart().getApplicationProtocolsList().size()!=1 ||
-                  req.getClientStart().getApplicationProtocols(0) != grpcAppProtocol){
-                    resp.setStatus(SessionStatus.newBuilder().setCode(Code.INVALID_ARGUMENT.value())
-                      .setDetails("application protocol was not grpc").build());
-                    return resp.build();
-                  }
-                if (req.getClientStart().getMaxTlsVersion() != TLSVersion.TLS1_3){
-                    resp.setStatus(SessionStatus.newBuilder().setCode(Code.INVALID_ARGUMENT.value())
-                      .setDetails("max TLS version must be 1.3").build());
-                    return resp.build();
-                }
-                resp.setOutFrames(ByteString.copyFrom(clientHelloFrame.getBytes()));
+              if (req.getClientStart().getMaxTlsVersion() != TLSVersion.TLS1_3){
+                  resp.setStatus(SessionStatus.newBuilder().setCode(Code.INVALID_ARGUMENT.value())
+                    .setDetails("max TLS version must be 1.3").build());
+                  return resp.build();
+              }
+              resp.setOutFrames(ByteString.copyFrom(clientHelloFrame.getBytes()));
+              resp.setBytesConsumed(0);
+              resp.setStatus(SessionStatus.newBuilder().setCode(Code.OK.value()).build());
+              localIdentity = req.getClientStart().getLocalIdentity();
+              if (req.getClientStart().getTargetIdentitiesList().size() > 0 ){
+                peerIdentity = req.getClientStart().getTargetIdentities(0);
+              }
+              assistingClient = true;
+              state = 2;
+              return resp.build();
+            }
+
+            private SessionResp processServerStart(SessionReq req){
+              SessionResp.Builder resp = SessionResp.newBuilder();
+              if (state != 0){
+                resp.setStatus(SessionStatus.newBuilder().setCode(Code.FAILED_PRECONDITION.value())
+                  .setDetails("server start handshaker not in initial state").build());
+                return resp.build();
+              }
+              if (req.getServerStart().getApplicationProtocolsList().size() != 1 ||
+                req.getServerStart().getApplicationProtocols(0) != grpcAppProtocol){
+                resp.setStatus(SessionStatus.newBuilder().setCode(Code.INVALID_ARGUMENT.value())
+                  .setDetails("application protocol was not grpc").build());
+                return resp.build();
+              }
+              if( req.getServerStart().getMaxTlsVersion() != TLSVersion.TLS1_3){
+                resp.setStatus(SessionStatus.newBuilder().setCode(Code.INVALID_ARGUMENT.value())
+                  .setDetails("max TLS version must be 1.3").build());
+                return resp.build();
+              }
+              if (req.getServerStart().getInBytes().size() == 0) {
                 resp.setBytesConsumed(0);
-                resp.setStatus(SessionStatus.newBuilder().setCode(Code.OK.value()).build());
-                localIdentity=req.getClientStart().getLocalIdentity();
-                if (req.getClientStart().getTargetIdentitiesList().size()>0 ){
-                  peerIdentity = req.getClientStart().getTargetIdentities(0);
-                }
-                assistingClient = true;
+                state = 1;
+              } else if (req.getServerStart().getInBytes().equals(ByteString.copyFrom(grpcAppProtocol.getBytes()))){
+                resp.setOutFrames(ByteString.copyFrom(serverFrame.getBytes()));
+                resp.setBytesConsumed(clientHelloFrame.length());
                 state = 2;
+              } else {
+                resp.setStatus(SessionStatus.newBuilder().setCode(Code.INTERNAL.value())
+                  .setDetails("server start request did not have the correct input bytes").build());
                 return resp.build();
               }
 
-              private SessionResp processServerStart(SessionReq req){
-                
-                SessionResp.Builder resp = SessionResp.newBuilder();
-                if (state!=0){
+              resp.setStatus(SessionStatus.newBuilder().setCode(Code.OK.value()).build());
+              if (req.getServerStart().getLocalIdentitiesList().size() > 0) {
+                peerIdentity = req.getServerStart().getLocalIdentities(0);
+              }
+              assistingClient = false;
+              return resp.build();
+            }
+
+            private SessionResp processNext(SessionReq req){
+              SessionResp.Builder resp = SessionResp.newBuilder();
+              if (assistingClient) {
+                if (state != 2) {
                   resp.setStatus(SessionStatus.newBuilder().setCode(Code.FAILED_PRECONDITION.value())
-                    .setDetails("server start handshaker not in initial state").build());
+                    .setDetails("clienthandshaker was not in sent state").build());
                   return resp.build();
                 }
-                if (req.getServerStart().getApplicationProtocolsList().size()!=1 ||
-                  req.getServerStart().getApplicationProtocols(0) != grpcAppProtocol){
-                    resp.setStatus(SessionStatus.newBuilder().setCode(Code.INVALID_ARGUMENT.value())
-                      .setDetails("application protocol was not grpc").build());
+                if (!req.getNext().getInBytes().equals(ByteString.copyFrom(serverFrame.getBytes()))){
+                  resp.setStatus(SessionStatus.newBuilder().setCode(Code.INTERNAL.value())
+                    .setDetails("client request did not match server frame").build());
+                  return resp.build();
+                }
+                resp.setOutFrames(ByteString.copyFrom(clientFinishedFrame.getBytes()));
+                resp.setBytesConsumed(serverFrame.length());
+                state = 3;
+              } else {
+                if (state == 1){
+                  if (!req.getNext().getInBytes().equals(ByteString.copyFrom(clientHelloFrame.getBytes()))){
+                    resp.setStatus(SessionStatus.newBuilder().setCode(Code.INTERNAL.value())
+                      .setDetails("server request did not match client hello frame").build());
                     return resp.build();
                   }
-                if( req.getServerStart().getMaxTlsVersion() != TLSVersion.TLS1_3){
-                    resp.setStatus(SessionStatus.newBuilder().setCode(Code.INVALID_ARGUMENT.value())
-                      .setDetails("max TLS version must be 1.3").build());
-                    return resp.build();
-                }
-                if (req.getServerStart().getInBytes().size() == 0) {
-                  resp.setBytesConsumed(0);
-                  state =1;
-                } else if (req.getServerStart().getInBytes().equals(ByteString.copyFrom(grpcAppProtocol.getBytes()))){
                   resp.setOutFrames(ByteString.copyFrom(serverFrame.getBytes()));
                   resp.setBytesConsumed(clientHelloFrame.length());
-                  state = 2;
-                } else{
-                  resp.setStatus(SessionStatus.newBuilder().setCode(Code.INTERNAL.value())
-                    .setDetails("server start request did not have the correct input bytes").build());
+                  state = 3;
+                } else if(state == 2) {
+                  if (!req.getNext().getInBytes().equals(ByteString.copyFrom(clientFinishedFrame.getBytes()))){
+                    resp.setStatus(SessionStatus.newBuilder().setCode(Code.INTERNAL.value())
+                      .setDetails("server request did not match client finished frame").build());
+                    return resp.build();
+                  }
+                  resp.setBytesConsumed(clientFinishedFrame.length());
+                  state = 4;
+                } else {
+                  resp.setStatus(SessionStatus.newBuilder().setCode(Code.FAILED_PRECONDITION.value())
+                    .setDetails("server request was not in expected state").build());
                   return resp.build();
                 }
-
-                resp.setStatus(SessionStatus.newBuilder().setCode(Code.OK.value()).build());
-                if (req.getServerStart().getLocalIdentitiesList().size()>0) {
-                  peerIdentity = req.getServerStart().getLocalIdentities(0);
-                }
-                assistingClient = false;
-                return resp.build();
               }
-
-              private SessionResp processNext(SessionReq req){
-                SessionResp.Builder resp = SessionResp.newBuilder();
-                if (assistingClient) {
-                  if (state != 2) {
-                    resp.setStatus(SessionStatus.newBuilder().setCode(Code.FAILED_PRECONDITION.value())
-                    .setDetails("clienthandshaker was not in sent state").build());
-                    return resp.build();
-                  }
-                  if (!req.getNext().getInBytes().equals(ByteString.copyFrom(serverFrame.getBytes()))){
-                    resp.setStatus(SessionStatus.newBuilder().setCode(Code.INTERNAL.value())
-                      .setDetails("client request did not match server frame").build());
-                    return resp.build();
-                  }
-                  resp.setOutFrames(ByteString.copyFrom(clientFinishedFrame.getBytes()));
-                  resp.setBytesConsumed(serverFrame.length());
-                  state = 3;
-                } else {
-                  if (state == 1){
-                    if (!req.getNext().getInBytes().equals(ByteString.copyFrom(clientHelloFrame.getBytes()))){
-                      resp.setStatus(SessionStatus.newBuilder().setCode(Code.INTERNAL.value())
-                        .setDetails("server request did not match client hello frame").build());
-                      return resp.build();
-                    }
-                    resp.setOutFrames(ByteString.copyFrom(serverFrame.getBytes()));
-                    resp.setBytesConsumed(clientHelloFrame.length());
-                    state = 3;
-                  }else if(state == 2) {
-                    if (!req.getNext().getInBytes().equals(ByteString.copyFrom(clientFinishedFrame.getBytes()))){
-                      resp.setStatus(SessionStatus.newBuilder().setCode(Code.INTERNAL.value())
-                        .setDetails("server request did not match client finished frame").build());
-                      return resp.build();
-                    }
-                    resp.setBytesConsumed(clientFinishedFrame.length());
-                    state = 4;
-                  } else {
-                    resp.setStatus(SessionStatus.newBuilder().setCode(Code.FAILED_PRECONDITION.value())
-                      .setDetails("server request was not in expected state").build());
-                    return resp.build();
-                  }
-                }
-                resp.setStatus(SessionStatus.newBuilder().setCode(Code.OK.value()).build());
-                if (state == 4){
-                  resp.setResult(getSessionResult());
-                }
-                return resp.build();
+              resp.setStatus(SessionStatus.newBuilder().setCode(Code.OK.value()).build());
+              if (state == 4){
+                resp.setResult(getSessionResult());
               }
+              return resp.build();
+            }
 
-              private SessionResult getSessionResult(){
-                SessionResult.Builder res = SessionResult.newBuilder();
-                res.setApplicationProtocol(grpcAppProtocol);
-                res.setState(SessionState.newBuilder().setTlsVersion(TLSVersion.TLS1_3)
-                  .setTlsCiphersuite(Ciphersuite.CHACHA20_POLY1305_SHA256)
-                  .setInKey(ByteString.copyFrom(inKey.getBytes()))
-                  .setOutKey(ByteString.copyFrom(outKey.getBytes())).build());
-                res.setPeerIdentity(peerIdentity);
-                res.setLocalIdentity(localIdentity);
-                return res.build();
-              }
-            };
+            private SessionResult getSessionResult(){
+              SessionResult.Builder res = SessionResult.newBuilder();
+              res.setApplicationProtocol(grpcAppProtocol);
+              res.setState(SessionState.newBuilder().setTlsVersion(TLSVersion.TLS1_3)
+                .setTlsCiphersuite(Ciphersuite.CHACHA20_POLY1305_SHA256)
+                .setInKey(ByteString.copyFrom(inKey.getBytes()))
+                .setOutKey(ByteString.copyFrom(outKey.getBytes())).build());
+              res.setPeerIdentity(peerIdentity);
+              res.setLocalIdentity(localIdentity);
+              return res.build();
+            }
+          };
         }
     }
 
